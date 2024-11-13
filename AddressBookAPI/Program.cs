90be356bc56670035bd3577454dd6e13ad36a0aa
ernestsmotlak <ernestsmotlak@gmail.com>
2024-11-13 19:00:06 +0100

    Made a specific user get statement.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- AddressBookAPI/Program.cs
+++ AddressBookAPI/Program.cs
@@ -12,15 +12,16 @@ var app = builder.Build();
 
 
 // Map endpoints here
-
 app.MapGet("/data", async (AppDbContext db) =>
 {
     return await db.Contacts.ToListAsync();
 });
 
-app.MapGet("/test", async (AppDbContext db) =>
-{
-    Console.WriteLine("Hi!");
+app.MapGet("/specificUser", async(AppDbContext db) => {
+    var specificUser = await db.Contacts.Where(contact => contact.FirstName == "Macek").ToListAsync();
+    return specificUser;
 });
+
+
 
 app.Run();
